a171a33caf8a24864fc2bbd8c27986fe906e0750
mathias5r <mathiassilva4@gmail.com>
2021-03-02 22:15:09 -0300

    test: ensure SocketSendMessage call SocketSender with correct values

---
+++ src/data/use-cases/send-message/socket-send-message.spec.ts
@@ -0,0 +1,40 @@
+import { SocketSendMessage } from './socket-send-message';
+import { SocketSender } from '../../protocols/socket/socket-sender';
+import { Message, SendMessage } from '../../../domain/send-message';
+
+const makeFakeMessage = (): Message => ({
+  instrument: 'piano',
+  node: 'A',
+});
+
+const makeSocketSenderStub = (): SocketSender => {
+  class SocketSenderStub implements SocketSender {
+    send(_: Message): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+  return new SocketSenderStub();
+};
+
+interface SutTypes {
+  socketSenderStub: SocketSender;
+  sendMessageSut: SendMessage;
+}
+
+const makeSut = (): SutTypes => {
+  const socketSenderStub = makeSocketSenderStub();
+  const sendMessageSut = new SocketSendMessage(socketSenderStub);
+  return {
+    socketSenderStub,
+    sendMessageSut,
+  };
+};
+
+describe('SendMessage UseCase', () => {
+  it('Should call SocketSender with correct values', async () => {
+    const { socketSenderStub, sendMessageSut } = makeSut();
+    const sendSpy = jest.spyOn(socketSenderStub, 'send');
+    await sendMessageSut.send(makeFakeMessage());
+    expect(sendSpy).toHaveBeenCalledWith(makeFakeMessage());
+  });
+});
