98720695b2253651c036032ad6fc0c195c1fcb1d
mathias5r <mathiassilva4@gmail.com>
2021-03-08 13:32:19 -0300

    test: ensure SocketIOAdapter calls callback in socket.on

--- src/infra/socket/socket-io-adapter.spec.ts
+++ src/infra/socket/socket-io-adapter.spec.ts
@@ -4,7 +4,9 @@ import { SocketIOAdapter } from './socket-io-adapter';
 jest.mock('socket.io-client', () => ({
   io: jest.fn().mockReturnValue({
     emit: jest.fn(),
-    on: jest.fn(),
+    on: jest
+      .fn()
+      .mockImplementation((_: string, callback: any) => callback({})),
   }),
 }));
 
@@ -28,5 +30,27 @@ describe('SocketIOAdapter', () => {
     const message = makeFakeMessage();
     sut.send(message);
     expect(socket.emit).toHaveBeenCalledWith(message.type, message.payload);
+  });
+
+  it('Should call on with correct values', () => {
+    const sut = new SocketIOAdapter();
+    const callback = jest.fn();
+    sut.receive(callback);
+    expect(socket.on).toHaveBeenNthCalledWith(
+      1,
+      'disconnect',
+      expect.any(Function),
+    );
+    expect(socket.on).toHaveBeenNthCalledWith(
+      2,
+      'connect',
+      expect.any(Function),
+    );
+    expect(socket.on).toHaveBeenNthCalledWith(3, 'error', expect.any(Function));
+    expect(socket.on).toHaveBeenNthCalledWith(
+      4,
+      'connect_error',
+      expect.any(Function),
+    );
   });
 });
