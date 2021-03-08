bf470f4499330067ddf3ac719019a1acc9d518c8
mathias5r <mathiassilva4@gmail.com>
2021-03-08 13:32:06 -0300

    feat: ensure SocketIOAdapter calls callback in socket.on

--- src/infra/helpers/socket-io-helper.ts
+++ src/infra/helpers/socket-io-helper.ts
@@ -3,24 +3,10 @@ import { io, Socket } from 'socket.io-client';
 export class SocketIOHelper {
   private static _instance: Socket;
 
-  private constructor() {}
-
   public static getInstance(): Socket {
     if (!this._instance) {
       this._instance = io(
-        process.env.URL_SOCKET_SERVER || 'http://192.168.0.23:3000',
-      );
-      this._instance.on('connect', () => {
-        console.log(`Socket ${this._instance.id} connected`);
-      });
-      this._instance.on('disconnect', () =>
-        console.log(`Socket ${this._instance.id} disconnected`),
-      );
-      this._instance.on('connect_error', (err: any) => {
-        console.log(`connect_error due to ${err.message}`);
-      });
-      this._instance.on('error', (error: any) =>
-        console.log(`Socket ${this._instance.id} error: ${error}`),
+        process.env.URL_SOCKET_SERVER || 'http://192.168.0.20:3000',
       );
     }
     return this._instance;
--- src/infra/socket/socket-io-adapter.ts
+++ src/infra/socket/socket-io-adapter.ts
@@ -1,11 +1,45 @@
-import { Message } from '../../domain/use-cases/send-message/send-message';
+import { Message } from './../../domain/use-cases/send-message/send-message';
+import { SocketReceiver } from './../../data/protocols/socket/socket-receiver';
 import { SocketSender } from './../../data/protocols/socket/socket-sender';
 import { SocketIOHelper } from '../helpers/socket-io-helper';
 
-export class SocketIOAdapter implements SocketSender {
+export class SocketIOAdapter implements SocketSender, SocketReceiver {
   send(message: Message): void {
     const socket = SocketIOHelper.getInstance();
     socket.emit(message.type, message.payload);
+    return;
+  }
+
+  receive(callback: (message: Message) => void): void {
+    const socket = SocketIOHelper.getInstance();
+    socket.on('disconnect', () =>
+      callback({
+        type: 'disconnect',
+        payload: null,
+      }),
+    );
+    socket.on('connect', () =>
+      callback({
+        type: 'connect',
+        payload: null,
+      }),
+    );
+    socket.on('error', (err: any) =>
+      callback({
+        type: 'error',
+        payload: {
+          error: err.message,
+        },
+      }),
+    );
+    socket.on('connect_error', (err: any) =>
+      callback({
+        type: 'connect_error',
+        payload: {
+          error: err.message,
+        },
+      }),
+    );
     return;
   }
 }
